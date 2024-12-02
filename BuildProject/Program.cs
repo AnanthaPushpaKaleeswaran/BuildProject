597966bc24b83378d303db12cde0c565499f2059
AnanthaPushpaKaleeswaran <ananthapushpakaleeswaran@gmail.com>
2024-12-02 16:14:34 +0530

    Create the project

---
+++ BuildProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BuildProject
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Build project");
+        }
+    }
+}
